e0946e85424a1362b1426b73ac5808b0ee38fc05
Ashley Kapaso <ashleykapaso@gmail.com>
2023-08-08 17:20:26 +0200

    add inital files

---
+++ Practical_2/Task_1/MyThread.java
@@ -0,0 +1,13 @@
+public class MyThread extends Thread {
+    SharedResources criticalSection;
+
+	public MyThread(SharedResources CrSection ){
+		criticalSection = CrSection;
+	}
+
+	@Override
+	public void run()
+	{
+
+	}
+}
