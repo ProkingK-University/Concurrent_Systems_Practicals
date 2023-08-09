5ac1b1f14d8955a752ecda3cff0965344c63263f
Ashley Kapaso <ashleykapaso@gmail.com>
2023-08-09 13:28:54 +0200

    impleament filter lock

--- Practical_2/Task_1/Filter.java
+++ Practical_2/Task_1/Filter.java
@@ -1,22 +1,62 @@
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.Condition;
 
 // Name: Ashley
 // Student Number: u21525600
 
 public class Filter implements Lock
 {
-	@Override
-	public void lock()
-	{
+	private int[] level;
+    private int[] victim;
+	private int numberOfThreads;
 
-	}
+	public Filter(int numberOfThreads)
+	{
+		level = new int[numberOfThreads];
+        victim = new int[numberOfThreads];
+        this.numberOfThreads = numberOfThreads;
+
+        for (int i = 0; i < numberOfThreads; i++)
+		{
+            level[i] = 0;
+        }
+    }
+
+	@Override
+    public void lock()
+	{
+        int i = (int) Thread.currentThread().threadId();
+
+        for (int l = 1; l < numberOfThreads; l++)
+		{
+            level[i] = l;
+            victim[l] = i;
+
+            boolean conflicting = true;
+
+            while (conflicting)
+			{
+                conflicting = false;
+
+                for (int k = 0; k < numberOfThreads; k++)
+				{
+                    if (k != i && level[k] >= l && victim[l] == i)
+					{
+                        conflicting = true;
+
+                        break;
+                    }
+                }
+            }
+        }
+    }
 
 	@Override
 	public void unlock()
 	{
-
+		int i = (int) Thread.currentThread().threadId();
+        level[i] = 0;
 	}
 
 	public boolean tryLock()
--- Practical_2/Task_1/SharedResources.java
+++ Practical_2/Task_1/SharedResources.java
@@ -3,9 +3,10 @@ import java.util.concurrent.locks.Lock;
 public class SharedResources
 {
     Lock l;
+	private static int counter = 0;
 
 	public void access()
 	{
-
+		counter = counter + 1;
 	}
 }
