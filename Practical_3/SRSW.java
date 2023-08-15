680d79978b7fac77b3f1a2cc8dd8deb14b5af696
Ashley Kapaso <ashleykapaso@gmail.com>
2023-08-15 20:58:19 +0200

    impleament srsw

--- Practical_3/SRSW.java
+++ Practical_3/SRSW.java
@@ -1,5 +1,42 @@
 package Practical_3;
 
-public class SRSW {
-    
+public class SRSW<T> implements Register<T> {
+    StampedValue<T> r_value;
+    ThreadLocal<Long> lastStamp;
+    ThreadLocal<StampedValue<T>> lastRead;
+
+    public SRSW(T value) {
+        r_value = new StampedValue<T>(value);
+
+        lastStamp = new ThreadLocal<Long>() {
+            protected Long initialValue() {
+                return 0L;
+            };
+        };
+
+        lastRead = new ThreadLocal<StampedValue<T>>() {
+            protected StampedValue<T> initialValue() {
+                return r_value;
+            };
+        };
+    }
+
+    @SuppressWarnings("unchecked")
+    public T read() {
+        StampedValue<T> value = r_value;
+        StampedValue<T> last = lastRead.get();
+        StampedValue<T> result = StampedValue.max(value, last);
+
+        lastRead.set(result);
+
+        return result.value;
+    }
+
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public void write(T value) {
+        long stamp = lastStamp.get() + 1;
+
+        r_value = new StampedValue(stamp, value);
+        lastStamp.set(stamp);
+    }
 }
