9a401681be47983b4d6582adda5fbaf2b5f7cadf
Ashley Kapaso <ashleykapaso@gmail.com>
2023-08-15 20:40:31 +0200

    impleament stamped value

--- Practical_3/StampedValue.java
+++ Practical_3/StampedValue.java
@@ -1,5 +1,28 @@
 package Practical_3;
 
-public class StampedValue {
-    
+public class StampedValue<T> {
+    public T value;
+    public long stamp;
+
+    @SuppressWarnings("rawtypes")
+    public static StampedValue MIN_VALUE = new StampedValue<>(null);
+
+    public StampedValue(T value) {
+        stamp = 0;
+        this.value = value;
+    }
+
+    public StampedValue(long stamp, T value) {
+        this.stamp = stamp;
+        this.value = value;
+    }
+
+    @SuppressWarnings("rawtypes")
+    public static StampedValue max(StampedValue x, StampedValue y) {
+        if (x.stamp > y.stamp) {
+            return x;
+        } else {
+            return y;
+        }
+    }
 }
