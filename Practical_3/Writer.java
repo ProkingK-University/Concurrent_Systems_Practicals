539ed2039fc23667db8d396eb88e8f960513fe3c
Ashley Kapaso <ashleykapaso@gmail.com>
2023-08-15 20:17:18 +0200

    impleament writer

---
+++ Practical_3/Writer.java
@@ -0,0 +1,21 @@
+package Practical_3;
+
+public class Writer<T> extends Thread {
+    T value;
+    Register<T> register;
+
+    public Writer(Register<T> register, T value) {
+        this.value = value;
+        this.register = register;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public void run() {
+        register.write(value);
+        System.out.println(this.getName() + " Wrote: " + value);
+    }
+}
