3f7bcd83511387d08e06f96a023c83e0f5c7a6f5
Ashley Kapaso <ashleykapaso@gmail.com>
2023-08-16 09:54:20 +0200

    update mrsw to use srsw

--- Practical_3/MRSW.java
+++ Practical_3/MRSW.java
@@ -1,7 +1,7 @@
 public class MRSW<T> implements Register<T> {
     int numOfReaders;
     ThreadLocal<Long> lastStamp;
-    private StampedValue<T>[][] valueTable;
+    private StampedValue<SRSW<T>>[][] valueTable;
 
     @SuppressWarnings("unchecked")
     public MRSW(T init, int readers) {
@@ -13,13 +13,13 @@ public class MRSW<T> implements Register<T> {
             };
         };
 
-        valueTable = (StampedValue<T>[][]) new StampedValue[readers][readers];
+        valueTable = (StampedValue<SRSW<T>>[][]) new StampedValue[readers][readers];
 
         StampedValue<T> value = new StampedValue<T>(init);
 
         for (int i = 0; i < readers; i++) {
             for (int j = 0; j < readers; j++) {
-                valueTable[i][j] = value;
+                valueTable[i][j] = (StampedValue<SRSW<T>>) value;
             }
         }
     }
@@ -28,7 +28,7 @@ public class MRSW<T> implements Register<T> {
     public T read() {
         int id = (int) (Thread.currentThread().threadId() % numOfReaders);
 
-        StampedValue<T> value = valueTable[id][id];
+        StampedValue<SRSW<T>> value = valueTable[id][id];
 
         for (int i = 0; i < valueTable.length; i++) {
             value = StampedValue.max(value, valueTable[i][id]);
@@ -42,9 +42,10 @@ public class MRSW<T> implements Register<T> {
             valueTable[id][i] = value;
         }
 
-        return (T) value;
+        return (T) value.value;
     }
 
+    @SuppressWarnings("unchecked")
     public void write(T v) {
         long stamp = lastStamp.get() + 1;
 
@@ -53,7 +54,7 @@ public class MRSW<T> implements Register<T> {
         StampedValue<T> value = new StampedValue<T>(stamp, v);
 
         for (int i = 0; i < valueTable.length; i++) {
-            valueTable[i][i] = value;
+            valueTable[i][i] = (StampedValue<SRSW<T>>) value;
         }
     }
 }
--- Practical_3/Main.java
+++ Practical_3/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Register<Boolean> register = new MRMW<Boolean>(1,true);
+        Register<Boolean> register = new MRSW<Boolean>(true,1);
         Reader<Boolean> reader = new Reader<>(register);
 
         reader.start();
